1c957ff3790315f7ec87a0673e4f137d3373ad14
Atharva_Rasal <Atharva.rasal2000@gmail.com>
2025-07-07 15:17:06 +0530

    Added start.sh and updated requirements with gunicorn

---
+++ backend/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running Migrations..."
+python manage.py migrate
+
+echo "Collecting Static Files..."
+python manage.py collectstatic --noinput
+
+echo "Starting Gunicorn Server..."
+gunicorn backend.wsgi:application --bind 0.0.0.0:$PORT
